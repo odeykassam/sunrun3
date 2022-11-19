dbff9a69d7228bf5975a97de5c0d55ccc018156e
Jose Quijada <118389901+JqSolar@users.noreply.github.com>
2022-11-19 17:23:30 -0500

    Pixel Script Installed

--- v4/survey-fb.php
+++ v4/survey-fb.php
@@ -314,14 +314,7 @@
   const _echo_post = window._echo_post = [];
 </script>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5SLK4FV');</script>
-<!-- End Google Tag Manager -->
-</script>
+<?php include('pixelscript.php'); ?>
 </head>
 <body class="v2-page en">
 <!-- Google Tag Manager (noscript) -->
