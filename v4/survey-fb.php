5f9b952dc2b08a4e255a8b5d1dd0def680b63fbd
Jose Quijada <118389901+JqSolar@users.noreply.github.com>
2022-11-19 17:12:56 -0500

    tag manaer installed

--- v4/survey-fb.php
+++ v4/survey-fb.php
@@ -314,20 +314,20 @@
   const _echo_post = window._echo_post = [];
 </script>
 
-
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'AW-10897705597');
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5SLK4FV');</script>
+<!-- End Google Tag Manager -->
 </script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'GTM-K287S83');
-</script></head>
+</head>
 <body class="v2-page en">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SLK4FV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="work-in-progress">
     <div class="progress-materializecss">
         <div class="indeterminate"></div>
