97fc39b5e6de8ebe784237a86f615ee70263a302
Jose Quijada <118389901+JqSolar@users.noreply.github.com>
2022-12-04 20:51:15 -0500

    track lead facebook

--- v4/thank-you.php
+++ v4/thank-you.php
@@ -1883,6 +1883,25 @@
           ttq.track('SubmitForm');
 		}(window, document, 'ttq');
 	</script>
+	
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '764912974948083');
+fbq('track', 'PageView');
+fbq('track', 'Lead');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=764912974948083&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
 </head>
 
 <body data-new-gr-c-s-check-loaded="8.904.0" data-gr-ext-installed="">
