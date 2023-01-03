e350961de9d582dfed2f48215c31913faa548867
Jose Quijada <118389901+JqSolar@users.noreply.github.com>
2023-01-03 18:05:24 -0500

    Update pixelscript.php

--- v4/pixelscript.php
+++ v4/pixelscript.php
@@ -16,6 +16,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-5SLK4FV');</script>
 <!-- End Google Tag Manager -->
 
+<script type="text/javascript" src="https://www.jerlnk.com/scripts/sdk/everflow.js"></script>
+<script type="text/javascript"> EF.click({
+		offer_id: EF.urlParameter('oid'),
+		affiliate_id: EF.urlParameter('affid'),
+		sub1: EF.urlParameter('sub1'),
+		sub2: EF.urlParameter('sub2'),
+		sub3: EF.urlParameter('sub3'),
+		sub4: EF.urlParameter('sub4'),
+		sub5: EF.urlParameter('sub5'),
+		uid: EF.urlParameter('uid'),
+		source_id: EF.urlParameter('source_id'),
+		transaction_id: EF.urlParameter('_ef_transaction_id'),
+	});
+</script>
+
 <!-- Tiktok Pixel Code -->
 <script>
 		!function (w, d, t) {
