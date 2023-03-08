7ea28d53087375295af4f985b20449cafd8fa9e7
Hazem <45760742+hazem1234566@users.noreply.github.com>
2023-03-09 00:47:08 +0300

    sdsaw

--- v4/index.php
+++ v4/index.php
@@ -64,6 +64,19 @@
         }
     </script>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VD2MHGNVSL%22%3E"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-VD2MHGNVSL');
+    </script>
+
     <script>
         // This sample uses the Autocomplete widget to help the user select a
         // place, then it retrieves the address components associated with that
